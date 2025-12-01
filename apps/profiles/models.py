4b2a7b72956442eb071c45d15ae91078f9c27139
Axel Díaz <diaz.axelio@gmail.com>
2025-12-01 15:06:41 -0300

    Removes Profile model
    Removes the Profile model as profile data is now stored directly within the User model.
    
    This simplifies the data structure and eliminates redundancy.

--- apps/profiles/models.py
+++
@@ -1,3 +0,0 @@
-# This app only contains views and URLs for the profile endpoint.
-# The actual profile data is stored in the User model (apps.users.models.User).
-# The Profile model has been removed as all data is now in User.
