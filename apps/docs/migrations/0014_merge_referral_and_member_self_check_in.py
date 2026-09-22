eba7ae4e3d342bc0c1a5608fb556cd0156d6c97c
Axel Díaz <diaz.axelio@gmail.com>
2026-09-22 19:46:45 -0300

    fix: merge docs migration branches

---
+++ apps/docs/migrations/0014_merge_referral_and_member_self_check_in.py
@@ -0,0 +1,10 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("docs", "0009_add_referral_guides"),
+        ("docs", "0013_add_member_self_check_in_guide"),
+    ]
+
+    operations = []
