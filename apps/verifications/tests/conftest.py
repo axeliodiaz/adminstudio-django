dd9111d43a5eb80a5f74dd00afec597b822a21d7
Axel Díaz <diaz.axelio@gmail.com>
2026-09-24 02:54:48 -0300

    CYC-75: pytest 9 - drop fixture-applied django_db marks

--- apps/verifications/tests/conftest.py
+++ apps/verifications/tests/conftest.py
@@ -6,13 +6,11 @@ from model_bakery import baker
 
 
 @pytest.fixture
-@pytest.mark.django_db
 def inactive_user():
     return baker.make("users.User", is_active=False)
 
 
 @pytest.fixture
-@pytest.mark.django_db
 def verification_code(inactive_user):
     return baker.make(
         "verifications.VerificationCode",
