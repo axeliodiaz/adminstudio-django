10fba1c423186c2777b06f6a96bf7236e45843cc
Axel Díaz <diaz.axelio@gmail.com>
2026-09-24 02:41:10 -0300

    CYC-75: drop legacy coreapi AutoSchema setting (removed in DRF 3.17)
    Remove default schema class from REST framework settings.

--- adminstudio_django/settings/local.py
+++ adminstudio_django/settings/local.py
@@ -8,7 +8,6 @@ REST_FRAMEWORK["DEFAULT_RENDERER_CLASSES"] = [
     "rest_framework.renderers.JSONRenderer",
     "rest_framework.renderers.BrowsableAPIRenderer",
 ]
-REST_FRAMEWORK["DEFAULT_SCHEMA_CLASS"] = "rest_framework.schemas.coreapi.AutoSchema"
 
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0", "testserver"]
